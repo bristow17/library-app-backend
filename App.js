ef704e28fff0f21ad20e639d058befee7de8238f
bristow17 <bristowgrorge17@gmail.com>
2024-06-13 05:50:22 +0530

    add funtion completed

--- App.js
+++ App.js
@@ -10,6 +10,18 @@ app.use(express.json())
 
 mongoose.connect("mongodb+srv://bristow17:bristow1904@cluster0.vn8j7z4.mongodb.net/librarynewdb?retryWrites=true&w=majority&appName=Cluster0")
 
+app.post("/add",(req,res)=>{
+
+    let input=req.body
+    let library=new librarymodel(input)
+    library.save()
+    res.json({"status":"success"})
+    
+})
+
+
+
+
 
 
 
